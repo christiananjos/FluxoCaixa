4ab9ac66269d46d105200fa77407caa5b7b6594d
Christian_Anjos <christiananjos@hotmail.com>
2023-12-28 13:31:30 -0300

    criação Models basicas

---
+++ FluxoCaixa/FluxoCaixa.Domain/Models/Conta.cs
@@ -0,0 +1,8 @@
+﻿namespace FluxoCaixa.Domain
+{
+    public class Conta
+    {
+        public int ContaId { get; set; }
+        public decimal Saldo { get; set; }
+    }
+}
---
+++ FluxoCaixa/FluxoCaixa.Domain/Models/Transacao.cs
@@ -0,0 +1,10 @@
+﻿namespace FluxoCaixa.Domain.Models
+{
+    public class Transacao
+    {
+        public int TransacaoId { get; set; }
+        public int ContaId { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
