1828bf9badfd62eec077905113912329ee497118
Christian_Anjos <christiananjos@hotmail.com>
2023-12-29 00:10:27 -0300

    implementação IServiceCollection

---
+++ FluxoCaixa/FluxoCaixa.Data/ServiceExtension/ServiceExtension.cs
@@ -0,0 +1,24 @@
+﻿using FluxoCaixa.Data.Interfaces;
+using FluxoCaixa.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FluxoCaixa.Data.ServiceExtension
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection AddDIServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<Context>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IContaRepository, ContaRepository>();
+            services.AddScoped<ITransacaoRepository, TransacaoRepository>();
+
+            return services;
+        }
+    }
+}
