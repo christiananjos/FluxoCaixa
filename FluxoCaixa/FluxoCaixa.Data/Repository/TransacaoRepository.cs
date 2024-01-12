b8b905c5693233e95eadb68685d618da444f0862
Christian_Anjos <christiananjos@hotmail.com>
2024-01-12 19:23:27 -0300

    implementação GetFilter. Obs: Irei melhorar esse where

--- FluxoCaixa/FluxoCaixa.Data/Repository/TransacaoRepository.cs
+++ FluxoCaixa/FluxoCaixa.Data/Repository/TransacaoRepository.cs
@@ -1,17 +1,29 @@
 ﻿using FluxoCaixa.Data.Interfaces;
 using FluxoCaixa.Domain.Entities;
 using FluxoCaixa.Domain.Input;
-using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace FluxoCaixa.Data.Repository
 {
     public class TransacaoRepository : BaseRepository<Transacao>, ITransacaoRepository
     {
-        public TransacaoRepository(IUnitOfWork unitOfWork) : base(unitOfWork) { }
+        private readonly IUnitOfWork _unitOfWork;
+        protected DbSet<Transacao> dbSet;
+        public TransacaoRepository(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            dbSet = _unitOfWork.Context.Set<Transacao>();
+        }
 
         public async Task<IEnumerable<Transacao>> GetFilter(TransacaoFilter transacao)
         {
-            throw new NotImplementedException();
+            var transacoes = await dbSet
+                .Where(x => transacao.ContaId == null || x.ContaId == transacao.ContaId)
+                .Where(x => transacao.TipoTransacaoId == null || x.TipoTransacaoId == transacao.TipoTransacaoId)
+                .Where(x => transacao.CreateAt == null || x.CreateAt == transacao.CreateAt)
+                .ToListAsync();
+
+            return transacoes;
         }
     }
 
